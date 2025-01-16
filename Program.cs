77a19a531335142d2bc1302435a9962b61e489d9
Daniel Gherghetta <gherghetta@gmail.com>
2025-01-16 13:36:10 +0100

    step 1 wporking adding tasks

--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ internal class Program
     private static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, World! press t for new task");
+        Console.WriteLine($"tasks {tasks.Count()}");
         while (true)
         {
             if(Console.KeyAvailable && Console.ReadKey(true).KeyChar == 't')
             {
-                Console.WriteLine($"tasks {tasks.Count(t => !t.IsCompleted)}");
                 AddTask();
             }
         }
@@ -18,19 +18,26 @@ internal class Program
 
     private static void AddTask()
     {
-        tasks.Add(AnImportantTask());
+        var task = AnImportantTask();
+        tasks.Add(task);
+        task.ContinueWith((task) => Finished(task));
+        Clear();
+    }
+
+    private static void Finished(Task task)
+    {
+        tasks.Remove(task);
         Clear();
     }
 
     private static async Task AnImportantTask()
     {
-        await Task.Delay(Random.Shared.Next(500, 10000));
+        await Task.Delay(Random.Shared.Next(50, 1000));
     }
 
     private static void Clear()
     {
         Console.CursorTop = Console.CursorTop - 1;
-        Console.WriteLine();
-        Console.CursorTop = Console.CursorTop - 1;
+        Console.WriteLine($"tasks {tasks.Count()}");
     }
 }
