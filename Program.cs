e32d9ff53431ed4c99c172eb64c6465f13a40115
Daniel Gherghetta <gherghetta@gmail.com>
2025-01-16 13:23:25 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    static private List<Task> tasks = new List<Task>();
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! press t for new task");
+        while (true)
+        {
+            if(Console.KeyAvailable && Console.ReadKey(true).KeyChar == 't')
+            {
+                Console.WriteLine($"tasks {tasks.Count(t => !t.IsCompleted)}");
+                AddTask();
+            }
+        }
+    }
+
+    private static void AddTask()
+    {
+        tasks.Add(AnImportantTask());
+        Clear();
+    }
+
+    private static async Task AnImportantTask()
+    {
+        await Task.Delay(Random.Shared.Next(500, 10000));
+    }
+
+    private static void Clear()
+    {
+        Console.CursorTop = Console.CursorTop - 1;
+        Console.WriteLine();
+        Console.CursorTop = Console.CursorTop - 1;
+    }
+}
