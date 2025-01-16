e42c38a25452f9e1d4c19fc10690281784179e01
Daniel Gherghetta <gherghetta@gmail.com>
2025-01-16 13:56:03 +0100

    cancelation

--- Program.cs
+++ Program.cs
@@ -3,41 +3,61 @@
 internal class Program
 {
     static private List<Task> tasks = new List<Task>();
+    private static CancellationTokenSource cts = new CancellationTokenSource();
+
     private static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, World! press t for new task");
         Console.WriteLine($"tasks {tasks.Count()}");
         while (true)
         {
-            if(Console.KeyAvailable && Console.ReadKey(true).KeyChar == 't')
+            if(Console.KeyAvailable)
             {
-                AddTask();
+                var key = Console.ReadKey(true).KeyChar;
+                if(key == 't')
+                    AddTask(cts.Token);
+                
+                if(key == 'c')
+                {
+                    cts.Cancel();
+                    cts = new CancellationTokenSource();
+                }
             }
         }
     }
 
-    private static void AddTask()
+    private static void AddTask(CancellationToken ct)
     {
-        var task = AnImportantTask();
+        var task = AnImportantTask(ct);
         tasks.Add(task);
         task.ContinueWith((task) => Finished(task));
-        Clear();
+        Write();
     }
 
     private static void Finished(Task task)
     {
         tasks.Remove(task);
-        Clear();
+        Write();
     }
 
-    private static async Task AnImportantTask()
+    private static async Task AnImportantTask(CancellationToken ct)
     {
-        await Task.Delay(Random.Shared.Next(50, 1000));
+        var ticks = Random.Shared.Next(1, 10);
+        const int tickLength = 500;
+        for(int i = 0; i < ticks; i++)
+        {
+            if(ct.IsCancellationRequested)
+                return;
+
+            await Task.Delay(tickLength);
+        } 
     }
 
-    private static void Clear()
+    private static void Write()
     {
         Console.CursorTop = Console.CursorTop - 1;
-        Console.WriteLine($"tasks {tasks.Count()}");
+        Console.WriteLine("");
+        Console.CursorTop = Console.CursorTop - 1;
+        Console.WriteLine($"tasks {tasks.Count()}          ");
     }
 }
